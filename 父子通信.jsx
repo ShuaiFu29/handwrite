b17b25299ff07453017679986d1247b55b434f5a
kangyi <kangyi@trip.com>
2026-08-25 19:05:33 +0800

    父子通信

--- 父子通信.jsx
+++ 父子通信.jsx
@@ -1,22 +1,34 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-function ChildA({count}){
-    return <div>当前数字：{count}</div>
+function ChildA({ count }) {
+  return <div>当前数字：{count}</div>
 }
 
-function ChildB({onAdd}){
-    return <button onClick={onAdd}>+1</button>
+function ChildB({ onAdd }) {
+  const [step, setStep] = useState(1);
+  return (
+    <div>
+      <input 
+        type="number" 
+        value={step} 
+        onChange={(e) => setStep(Number(e.target.value))}
+      />
+      <button onClick={() => onAdd(step)}>按这个数值增加</button>
+    </div>
+  )
 }
 
-export default function Parent(){
-    let [count,setCount]=useState(0)
-    let handleAdd=()=>{
-        setCount(pre=>pre+1)
-    }
-    return (
-        <div>
-            <ChildA count={count}/>
-            <ChildB onAdd={handleAdd}/>
-        </div>
-    )
+export default function Parent() {
+  const [count, setCount] = useState(0);
+
+  const handleAdd = (step) => {
+    setCount(prev => prev + step);
+  };
+
+  return (
+    <div>
+      <ChildA count={count} />
+      <ChildB onAdd={handleAdd} />
+    </div>
+  )
 }
